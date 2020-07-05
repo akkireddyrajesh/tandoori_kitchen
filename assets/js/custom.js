c9e7b3d282ca51d278e5bbd088113c305e6da36b
rajesh <rajesha@onemoney.in>
2020-07-05 19:49:55 +0530

    client chnages

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,16 @@
 $('document').ready(function(){
-// $('.menu_ul').
+let menuList=[];
+const current_path = window.location.pathname.split('/').pop();
+console.log(`current file name from URL is :: ${current_path}`);
+$('.menu_ul > li').each(function(i, obj) {
+    //test
+    let dataUrl=$(this).attr('data-url');
+    menuList.push(dataUrl)
+    if(dataUrl==current_path){
+        $(this).addClass("btn_active");
+    }
+});
+console.log(`menuList loaded...`, menuList);
+
+$()
 });
