3e7ed67e2c0af0484cd045a818994aab054757c7
rajesh <rajesha@onemoney.in>
2020-07-18 09:27:36 +0530

    selected branch showing implemented

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,16 +1,20 @@
-$('document').ready(function(){
-let menuList=[];
-const current_path = window.location.pathname.split('/').pop();
-console.log(`current file name from URL is :: ${current_path}`);
-$('.menu_ul > li').each(function(i, obj) {
-    //test
-    let dataUrl=$(this).attr('data-url');
-    menuList.push(dataUrl)
-    if(dataUrl==current_path){
-        $(this).addClass("btn_active");
-    }
+$('document').ready(function () {
+    const current_path = window.location.pathname.split('/').pop();
+    const getBranchID = window.location.hash;
+    activeMenuLi(current_path);
+    activaTab(getBranchID);
 });
-console.log(`menuList loaded...`, menuList);
 
-$()
-});
+function activaTab(branchId) {
+    console.log(`getBranch from url is ${branchId}`);
+    $('.nav-tabs a[href="' + branchId + '"]').tab('show');
+}
+function activeMenuLi(current_path) {
+    console.log(`current file name from URL is :: ${current_path}`);
+    $('.menu_ul > li').each(function (i, obj) {
+        let dataUrl = $(this).attr('data-url');
+        if (dataUrl == current_path) {
+            $(this).addClass("btn_active");
+        }
+    });
+}
